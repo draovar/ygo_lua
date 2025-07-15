3886e1526fa0d8ed5602ca3915a5177e86898ea4
draovar <kkarachanov@gmail.com>
2025-07-15 22:28:34 +0300

    add tiles and camera

--- main.lua
+++ main.lua
@@ -1,13 +1,23 @@
 require "player";
 
 function love.load()
+    sti = require 'sti'
+    gameMap = sti('testMap.lua')
+
+    camera = require "camera";
+    cam = camera()
 end
 
 function love.draw()
     love.graphics.clear({0.5, 0.5, 1, 1})
-    player_draw()
+    cam:attach()
+        gameMap:drawLayer(gameMap.layers["ground"])
+        gameMap:drawLayer(gameMap.layers["objs"])
+        player_draw()
+    cam:detach()
 end
 
 function love.update(dt)
     player_update(dt)
+    cam:lookAt(math.floor(player.x), math.floor(player.y))
 end
