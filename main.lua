c29165e6139abcf632e74b3af11763bb67c57994
kkarachanov <kkarachanov@meta.com>
2025-07-17 05:53:08 -0700

    Y-Sorting 1

--- main.lua
+++ main.lua
@@ -2,8 +2,9 @@ require "player";
 
 function love.load()
     sti = require 'libs/sti'
-    gameMap = sti('maps/testMap.lua')
+    gameMap = sti('maps/map1.lua')
     camera = require "libs/camera";
+    objectsLayer = gameMap.layers["objects"]
     cam = camera()
 end
 
@@ -11,12 +12,47 @@ function love.draw()
     love.graphics.clear({0.5, 0.5, 1, 1})
     cam:attach()
         gameMap:drawLayer(gameMap.layers["ground"])
-        gameMap:drawLayer(gameMap.layers["objs"])
+
+        for _, obj in ipairs(objectsLayer.objects) do
+            if obj.y + obj.height < player.y + 32 then
+                local draw_x = 1 + obj.x/48
+                local draw_y = 1 + obj.y/48
+                local layer = gameMap.layers["objects_tiles"]
+                for i=0, obj.width/48, 1 do
+                    for j=0, obj.height/48, 1 do
+                        local tile = layer.data[draw_y + j][draw_x + i] -- Y, X
+                        if tile then
+                            local tileset = gameMap.tilesets[tile.tileset]
+                            love.graphics.draw(tileset.image, tile.quad, obj.x + i*48, obj.y + j*48)
+                        end
+                    end
+                end
+            end
+        end
+
         player_draw()
+
+        for _, obj in ipairs(objectsLayer.objects) do
+            if obj.y + obj.height > player.y + 32 then
+                local draw_x = 1 + obj.x/48
+                local draw_y = 1 + obj.y/48
+                local layer = gameMap.layers["objects_tiles"]
+                for i=0, obj.width/48, 1 do
+                    for j=0, obj.height/48, 1 do
+                        local tile = layer.data[draw_y + j][draw_x + i] -- Y, X
+                        if tile then
+                            local tileset = gameMap.tilesets[tile.tileset]
+                            love.graphics.draw(tileset.image, tile.quad, obj.x + i*48, obj.y + j*48)
+                        end
+                    end
+                end
+            end
+        end
     cam:detach()
 
     -- debug
     love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+    love.graphics.print("P_XY: "..tostring(player.x)..", "..tostring(player.y), 10, 30)
 end
 
 function love.update(dt)
